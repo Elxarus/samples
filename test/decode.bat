a1a192deb91b8e25014306317bbbbcf50ae05b0e
Valex <Valex@localhost>
2006-03-14 20:26:35 +0000

    test samples: decode test script changed due to new valdec path

--- test/decode.bat
+++ test/decode.bat
@@ -1,11 +1,11 @@
-..\valdec\release\valdec test.mp2       -i -d > decode.log
-..\valdec\release\valdec test.mp2.pes   -i -d >> decode.log
-..\valdec\release\valdec test.mp2.spdif -i -d >> decode.log
+..\..\valib\valdec\release\valdec test.mp2       -i -d > decode.log
+..\..\valib\valdec\release\valdec test.mp2.pes   -i -d >> decode.log
+..\..\valib\valdec\release\valdec test.mp2.spdif -i -d >> decode.log
 
-..\valdec\release\valdec test.ac3       -i -d >> decode.log
-..\valdec\release\valdec test.ac3.pes   -i -d >> decode.log
-..\valdec\release\valdec test.ac3.spdif -i -d >> decode.log
+..\..\valib\valdec\release\valdec test.ac3       -i -d >> decode.log
+..\..\valib\valdec\release\valdec test.ac3.pes   -i -d >> decode.log
+..\..\valib\valdec\release\valdec test.ac3.spdif -i -d >> decode.log
 
-..\valdec\release\valdec test.dts       -i -d >> decode.log
-rem ..\valdec\release\valdec test.dts.pes   -i -d >> decode.log
-..\valdec\release\valdec test.dts.spdif -i -d >> decode.log
+..\..\valib\valdec\release\valdec test.dts       -i -d >> decode.log
+rem ..\..\valib\valdec\release\valdec test.dts.pes   -i -d >> decode.log
+..\..\valib\valdec\release\valdec test.dts.spdif -i -d >> decode.log
